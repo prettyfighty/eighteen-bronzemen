82183d0eaa17fdd462ce26f39be6b0dfe7f7524d
lailai <prettyfighty@gmail.com>
2021-06-26 11:22:59 +0800

    add template (show) and delete unnecessary codes

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,7 +1,6 @@
 class Mission < ApplicationRecord
   # belongs_to :user
 
-  # enum priorities: { "1": "高", "2": "中", "3": "低" }
   enum priorities: { "高": 1, "中": 2, "低": 3 }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_25_152256) do
+ActiveRecord::Schema.define(version: 2021_06_26_031745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "missions", force: :cascade do |t|
     t.string "title"
-    t.string "content"
     t.string "tag"
     t.string "status"
     t.datetime "start_at"
     t.datetime "end_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "priority"
+    t.integer "priority"
+    t.text "content"
   end
 
   create_table "users", force: :cascade do |t|
