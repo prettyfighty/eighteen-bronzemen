8a7da8d5d95c95d9bf869c987d70aaa8cd404f18
lailai <prettyfighty@gmail.com>
2021-06-30 18:20:38 +0800

    feature spec - Unable to CRUD others' missions

--- spec/features/mission_spec.rb
+++ spec/features/mission_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 feature "mission list", type: :feature do
   before do
-    u1 = User.create(email: "user@gmail.com", password: "password")
+    u1 = User.create(email: "user1@gmail.com", password: "password")
+    u2 = User.create(email: "user2@gmail.com", password: "password")
     m1 = u1.missions.create(title: "m1", content: "m1_content", start_at: Time.new(2062, 6, 27, 9, 50, 0), end_at: Time.new(2062, 7, 28, 10, 0, 0), status: 1, priority: 1)
     m2 = u1.missions.create(title: "m2", content: "m2_content", start_at: Time.new(2062, 6, 29, 9, 50, 0), end_at: Time.new(2062, 7, 26, 10, 0, 0), status: 0, priority: 3)
     m3 = u1.missions.create(title: "m3", content: "m3_content", start_at: Time.new(2062, 6, 28, 9, 50, 0), end_at: Time.new(2062, 7, 27, 10, 0, 0), status: 2, priority: 2)
@@ -10,7 +11,7 @@ feature "mission list", type: :feature do
     visit "/"
     # 未登入狀態前往首頁，會導向登入頁面，在這邊先登入
     within("#new_user") do
-      fill_in "user_email", with: "user@gmail.com"
+      fill_in "user_email", with: "user1@gmail.com"
       fill_in "user_password", with: "password"
     end
     click_button "commit"
@@ -59,6 +60,18 @@ feature "mission list", type: :feature do
     click_button "commit"
 
     expect(page.body).to have_content("m3")
+    expect(page.body).not_to have_content("m2")
+  end
+
+  scenario "Unable to CRUD others missions" do
+    click_link I18n.t("sign_out")
+
+    within("#new_user") do
+      fill_in "user_email", with: "user2@gmail.com"
+      fill_in "user_password", with: "password"
+    end
+    click_button "commit"
+
     expect(page.body).not_to have_content("m2")
   end
 
