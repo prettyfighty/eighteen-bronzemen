68804cb0c63dcc73ac42bfbb5097ae2fa904f76f
lailai <prettyfighty@gmail.com>
2021-06-30 16:59:37 +0800

    feat - encrypt password before create User & beautify sign_up page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,19 @@ Rails.application.routes.draw do
 
   resources :missions
 
+  resources :users, path: "user", only: [] do
+    collection do
+      get :sign_up, action: 'new'
+      post :sign_up, action: 'create'
+    end
+  end
+
+  resources :sessions, path: "user", only: [] do
+    collection do
+      get :sign_in, action: "new"
+      post :sign_in, action: "create"
+      delete :sign_out, action: "destroy"
+    end
+  end
+
 end
